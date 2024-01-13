46eeee20ac0e42b22bb8076bb6fce6ffdc6f590f
Sadman600 <sadmansakib600@gmail.com>
2024-01-13 21:08:24 +0600

    add logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import MyButton from "./components/MyButton";
 import Profile from "./components/Profile";
 
 function App() {
+  const [content, setContent] = useState(false);
   return (
     <>
       <div className="AppContainer">
+        <button onClick={() => setContent((pre) => !pre)}>click me!</button>
         <h1>Welcome to my app</h1>
-        <MyButton />
-        <Profile />
+        {content ? <Profile /> : <MyButton />}
       </div>
     </>
   );
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,7 +10,15 @@ const Profile = () => {
   return (
     <>
       <h1>{user.name}</h1>
-      <img className="avatar" src={user.imageUrl} />
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
     </>
   );
 };
