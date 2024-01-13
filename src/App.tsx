d1185bee909e08dc781bba12cf52490301c85822
Sadman600 <sadmansakib600@gmail.com>
2024-01-13 21:20:51 +0600

    added products

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import MyButton from "./components/MyButton";
 import Profile from "./components/Profile";
+import Products from "./components/Products";
 
 function App() {
   const [content, setContent] = useState(false);
@@ -11,6 +12,7 @@ function App() {
         <button onClick={() => setContent((pre) => !pre)}>click me!</button>
         <h1>Welcome to my app</h1>
         {content ? <Profile /> : <MyButton />}
+        <Products />
       </div>
     </>
   );
